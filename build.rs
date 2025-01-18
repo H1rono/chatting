e98ed8f09f8a9d8cf992902d05cd6087ecf443d8
H1rono <hronok66@gmail.com>
2025-01-18 23:51:00 +0900

    Remove `derive(De, Ser)` in tonic-generated types

--- build.rs
+++ build.rs
@@ -6,7 +6,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(false)
         .build_server(true)
         .build_transport(true)
-        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
         .compile_protos(&[proto_dir.join("chatting.proto")], &[proto_dir])?;
     Ok(())
 }
