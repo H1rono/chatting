25b14b61c7670388d2b7698cb2a4a8fc348e6ffe
H1rono <hronok66@gmail.com>
2025-01-19 04:59:40 +0900

    Write main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
+use std::sync::Arc;
+
 use anyhow::Context;
+use futures::TryFutureExt;
 use sqlx::MySqlPool;
 
 use chatting::user::UserServiceImpl;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let env_filter =
+        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+
+    let pool = load_mysql_from_env("MYSQL_")
+        .or_else(|_| load_mysql_from_env("MARIADB_"))
+        .or_else(|_| load_mysql_from_env("NS_MARIADB_"))
+        .await?;
+    let user_service = UserServiceImpl;
+    let state = Arc::new(State { pool, user_service });
+    state.migrate().await?;
+    let router = chatting::router::make_router(state);
+    let make_service = axum::ServiceExt::into_make_service(router);
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| 8080.to_string())
+        .parse()
+        .context("Failed to read PORT value")?;
+    let addr: std::net::SocketAddr = ([0, 0, 0, 0], port).into();
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .with_context(|| format!("Failed to bind {addr}"))?;
+    tracing::info!(%addr, "Listening");
+    axum::serve(listener, make_service).await?;
+    Ok(())
 }
 
 #[derive(Debug, Clone)]
@@ -12,12 +39,13 @@ struct State {
     pool: MySqlPool,
     user_service: UserServiceImpl,
 }
+
 async fn load_mysql_from_env(prefix: &str) -> anyhow::Result<MySqlPool> {
     macro_rules! var {
-        ($n:ident) => {
-            std::env::var(&format!(concat!("{}", stringify!($n)), prefix))
-                .context(concat!("Failed to read", stringify!($n)))
-        };
+        ($n:ident) => {{
+            let var_name = format!(concat!("{}", stringify!($n)), prefix);
+            std::env::var(&var_name).with_context(|| format!("Failed to read {var_name}"))
+        }};
     }
 
     let hostname = var!(HOSTNAME)?;
