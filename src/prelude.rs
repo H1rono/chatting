cfd3e8665457884648d2b68afc6928f80372a223
H1rono <hronok66@gmail.com>
2025-01-20 10:29:27 +0900

    Add `prelude::Error`

---
+++ src/prelude.rs
@@ -0,0 +1,12 @@
+pub use tonic::{Code, Status};
+
+pub trait Error: std::error::Error + Send + Sync + 'static {
+    fn code(&self) -> Code;
+    fn message(&self) -> String;
+
+    fn to_status(&self) -> Status {
+        let code = self.code();
+        let message = self.message();
+        Status::new(code, message)
+    }
+}
