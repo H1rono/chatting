67ffb2d5b0ac0a93bbc624a0b2b9fbd3355f6768
H1rono <hronok66@gmail.com>
2025-02-27 12:41:19 +0900

    Edit prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,21 +1,11 @@
 pub use tonic::{Code, Status};
 
+pub use crate::error::Failure;
 pub type Timestamp = chrono::DateTime<chrono::Utc>;
 
-pub fn convert_timestamp(t: Timestamp) -> Result<prost_types::Timestamp, tonic::Status> {
+pub fn convert_timestamp(t: Timestamp) -> Result<prost_types::Timestamp, Failure> {
     let seconds = t.timestamp();
     let nanos = t.timestamp_subsec_nanos() as i32;
     let t = prost_types::Timestamp { seconds, nanos };
     Ok(t)
-}
-
-pub trait Error: std::error::Error + Send + Sync + 'static {
-    fn code(&self) -> Code;
-    fn message(&self) -> String;
-
-    fn to_status(&self) -> Status {
-        let code = self.code();
-        let message = self.message();
-        Status::new(code, message)
-    }
 }
